08a9429f16d74e780996c403bac27327f0470f83
AlexCoilaJrt <alex.coila.jrt@gmail.com>
2025-05-30 22:22:23 -0500

    asdasd

--- karma.conf.js
+++ karma.conf.js
@@ -1,42 +1,70 @@
 // karma.conf.js
 module.exports = function (config) {
-    config.set({
-      basePath: '',
-      frameworks: ['jasmine', '@angular-devkit/build-angular'],
-      plugins: [
-        require('karma-jasmine'),
-        require('karma-chrome-launcher'),
-        require('karma-jasmine-html-reporter'),
-        require('karma-coverage'),
-        require('@angular-devkit/build-angular/plugins/karma')
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    client: {
+      jasmine: {
+        random: true
+      },
+      clearContext: false
+    },
+    jasmineHtmlReporter: {
+      suppressAll: true
+    },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage/capachica-app'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' },
+        { type: 'lcov' }, // Cambiado de 'lcovonly' a 'lcov'
+        { type: 'cobertura' } // Opcional: formato adicional para CI/CD
       ],
-      client: {
-        jasmine: {
-          random: true
+      // Configuración adicional para asegurar la generación del archivo
+      fixWebpackSourcePaths: true,
+      skipFilesWithNoCoverage: false,
+      'report-config': {
+        'html': {
+          subdir: 'html'
         },
-        clearContext: false
-      },
-      jasmineHtmlReporter: {
-        suppressAll: true
-      },
-      coverageReporter: {
-        dir: require('path').join(__dirname, './coverage/capachica-app'), // carpeta donde quieres que guarde
-        subdir: '.', // usa el directorio raíz dentro de 'coverage/capachica-app'
-        reporters: [
-          { type: 'html' },
-          { type: 'lcovonly', subdir: '.', file: 'lcov.info' },  // <- para generar lcov.info aquí
-          { type: 'text-summary' },
-        ],
-      },
-      
-      reporters: ['progress', 'kjhtml', 'coverage'],
-      port: 9876,
-      colors: true,
-      logLevel: config.LOG_INFO,
-      autoWatch: true,
-      browsers: ['Chrome'],
-      singleRun: false,
-      restartOnFileChange: true
-    });
-  };
-  
+        'lcov': {
+          subdir: 'lcov'
+        }
+      }
+    },
+    reporters: ['progress', 'kjhtml', 'coverage'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['Chrome'],
+    singleRun: false,
+    restartOnFileChange: true,
+    
+    // Configuración adicional para asegurar que la cobertura funcione correctamente
+    preprocessors: {
+      // Esta configuración es manejada por Angular CLI, pero puedes especificarla si es necesario
+    },
+    
+    // Configuración para headless testing (opcional)
+    customLaunchers: {
+      ChromeHeadlessCustom: {
+        base: 'ChromeHeadless',
+        flags: [
+          '--no-sandbox',
+          '--disable-gpu',
+          '--disable-web-security',
+          '--disable-features=VizDisplayCompositor'
+        ]
+      }
+    }
+  });
+};
